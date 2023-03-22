6b82d60597b88a05c0c2e246a4737255369b5489
Claus Damgaard <jinxxed@outlook.com>
2023-03-22 21:39:02 +0100

    Minor output change.

--- Program.cs
+++ Program.cs
@@ -3,5 +3,5 @@
 Book b = new Book("Stephen King", "Carrie", new DateOnly(1974, 4, 5), 199);
 
 
-Console.WriteLine("Author: " + b.Author + " Title: " + b.Title + " Release date: " + b.ReleaseDate + " Pages: " + b.Pages);
+Console.WriteLine("Author: " + b.Author + ". Title: " + b.Title + ". Release date: " + b.ReleaseDate + ". Pages: " + b.Pages + ".");
 Console.WriteLine(b.Read(10));
