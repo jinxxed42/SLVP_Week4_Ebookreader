4e61870c806290d90534163559108a9dea1bb2be
Claus Damgaard <jinxxed@outlook.com>
2023-03-22 21:36:44 +0100

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLVP_Week4_Ebookreader
+{
+    internal class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string author, string title, DateTime releaseDate, int pages)
+        {
+            Author = author;
+            Title = title;
+            ReleaseDate = releaseDate;
+            Pages = pages;
+        }
+ 
+
+        public string Read (int numPages)
+        {
+            string result = "";
+            for (int i = 1; i <= numPages; i++)
+            {
+                result += "Page " + i + ". ";
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using SLVP_Week4_Ebookreader;
+
+Book b = new Book("Stephen King", "Carrie", new DateTime(1974, 4, 5), 199);
+
+
+Console.WriteLine("Author: " + b.Author + " Title: " + b.Title + " Release date: " + b.ReleaseDate + " Pages: " + b.Pages);
+Console.WriteLine(b.Read(10));
